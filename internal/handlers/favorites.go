d9365e02f159508f478b7007e6084bb582ba9c19
Amanjol <amanjol98@gmail.com>
2026-08-28 20:09:07 +0500

    update fav

--- internal/handlers/favorites.go
+++ internal/handlers/favorites.go
@@ -2,8 +2,10 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 	"ozinshe/internal/middleware"
+	"ozinshe/internal/repositories"
 	"ozinshe/internal/services"
 	"strconv"
 )
@@ -47,5 +49,27 @@ func (h *FavoriteHandler) AddFavoriteMovieToUser(w http.ResponseWriter, r *http.
 }
 
 func (h *FavoriteHandler) GetFavoriteMovies(w http.ResponseWriter, r *http.Request) {
-	userID, err := r.Context().Value(middleware.UserIDKey).(int)
+	userID, ok := r.Context().Value(middleware.UserIDKey).(int)
+	if !ok {
+		http.Error(w, "Пользователь не найден", http.StatusUnauthorized)
+		return
+	}
+
+	favoriteMovieResponse, err := h.service.GetFavoriteMovies(r.Context(), userID)
+	if err != nil {
+		if errors.Is(err, repositories.ErrUserNotFound) {
+			http.Error(w, "Пользователь не найден", http.StatusNotFound)
+			return
+		}
+
+		http.Error(w, "Внутренняя ошибка сервера", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(favoriteMovieResponse); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
